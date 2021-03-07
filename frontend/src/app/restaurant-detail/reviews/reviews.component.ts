e14161723a16f6e4f5d8e68aef836d3fc7341004
jacolpn <jacolpn@outlook.com>
2021-03-07 17:06:16 -0300

    refatored review component

--- frontend/src/app/restaurant-detail/reviews/reviews.component.ts
+++ frontend/src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,27 +1,29 @@
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 import { VILLE_API } from './../../app.api';
 import { Restaurant } from '../restaurant.model';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html'
 })
 export class ReviewsComponent implements OnInit {
-
   reviews: Observable<any>;
 
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.reviews = this.http
-      .get<Restaurant>(`${VILLE_API}/restaurants/bread-bakery/reviews`);
+    this.getDB();
   }
 
+  getDB() {
+    this.reviews = this.http
+      .get<Restaurant>(`${VILLE_API}/restaurants/bread-bakery/reviews`)
+      .pipe(tap(console.log));
+  }
+
+  mockDB() { }
 }
