bdf4f48b008db89ab09cd6397ed35e6db27e7b32
jacolpn <jacolpn@outlook.com>
2021-03-07 18:05:14 -0300

    added mock in menu

--- frontend/src/app/restaurant-detail/menu/menu.component.ts
+++ frontend/src/app/restaurant-detail/menu/menu.component.ts
@@ -10,17 +10,47 @@ import { Restaurant } from '../restaurant.model';
   templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit {
-  
-  menu: Observable<any>;
+  // menu: Observable<any>;
+  menu: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getDB();
+    // this.getDB();
+    this.mockDB();
   }
 
   getDB() {
     this.menu = this.http
       .get<Restaurant>(`${VILLE_API}/restaurants/bread-bakery/menu`)
+  }
+
+  mockDB() {
+    this.menu = [
+      {
+        "id": "cup-cake",
+        "imagePath": "assets/img/foods/cupcake.png",
+        "name": "Cup Cake",
+        "description": "Cup Cake recheado de Doce de Leite",
+        "price": 8.7,
+        "restaurantId": "bread-bakery"
+      },
+      {
+        "id": "donut",
+        "imagePath": "assets/img/foods/donut.png",
+        "name": "Donut",
+        "description": "Coberto com chantilly",
+        "price": 2.5,
+        "restaurantId": "bread-bakery"
+      },
+      {
+        "id": "bread",
+        "imagePath": "assets/img/foods/bread.png",
+        "name": "Pão Artesanal Italiano",
+        "description": "Pão artesanal com queijos italianos",
+        "price": 15.9,
+        "restaurantId": "bread-bakery"
+      }
+    ];
   }
 }
