ee1a70a74628586426f735295e7ed29601d27a7f
jacolpn <jacolpn@outlook.com>
2021-03-07 02:42:17 -0300

    removed dependencies and added style in user-detail

--- src/app/header/user-detail/user-detail.component.ts
+++ src/app/header/user-detail/user-detail.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html'
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
   user: string = "Jackson";
