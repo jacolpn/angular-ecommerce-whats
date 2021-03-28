5f8e5eefe3fe8a65501b6379e8f7808360da734b
jacolpn <jacolpn@outlook.com>
2021-03-28 03:07:12 -0300

    implemented number validation

--- frontend/src/app/order/order.component.ts
+++ frontend/src/app/order/order.component.ts
@@ -55,6 +55,20 @@ export class OrderComponent implements OnInit {
     });
   }
 
+  onlynumber(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    
+    key = String.fromCharCode(key);
+
+    var regex = /^[0-9.]+$/;
+
+    if( !regex.test(key) ) {
+       theEvent.returnValue = false;
+       if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+ }
+
   itemsValue(): number {
     return this.orderService.itemsValue();
   }
